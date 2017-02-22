4bf091bc46776145ce67d9a154f2fd09c017d8de
Marcos del Amo Salgado <marcosdelamo86@gmail.com>
2017-02-22 15:54:37 +0100

    Map y flatMap

---
+++ Java8_Tutorials/src/main/java/j8t/leccion10_flatmap/Persona.java
@@ -0,0 +1,64 @@
+package j8t.leccion10_flatmap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Class Persona.
+ * Link ejemplo:http://www.arquitecturajava.com/java-8-flatmap/
+ */
+public class Persona {
+
+	/** The nombre. */
+	private String nombre;
+	
+	/** The lista. */
+	private List<Viaje> lista= new ArrayList<Viaje>();
+	
+	/**
+	 * Gets the nombre.
+	 *
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * Adds the viaje.
+	 *
+	 * @param v the v
+	 */
+	public void addViaje(Viaje v) {
+		lista.add(v);
+	}
+	
+	/**
+	 * Gets the lista.
+	 *
+	 * @return the lista
+	 */
+	public List<Viaje> getLista() {
+		return lista;
+	}
+
+	/**
+	 * Instantiates a new persona.
+	 *
+	 * @param nombre the nombre
+	 */
+	public Persona(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	/**
+	 * Sets the nombre.
+	 *
+	 * @param nombre the new nombre
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
